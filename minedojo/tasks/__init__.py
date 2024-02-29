4793423093bf4c5e403328838930d562d9d934c8
laithaustin <laithaustin@utexas.edu>
2024-02-29 17:25:21 -0600

    fixed dependency issue with import_resource

--- minedojo/tasks/__init__.py
+++ minedojo/tasks/__init__.py
@@ -26,8 +26,12 @@ _logger.addHandler(_stream_handler)
 
 
 def _resource_file_path(fname) -> str:
-    with importlib_resources.path("minedojo.tasks.description_files", fname) as p:
+    with importlib_resources.as_file(
+        importlib_resources.files("minedojo.tasks.description_files").joinpath(fname)
+    ) as p:
         return str(p)
+    
+    
 
 
 _MetaTaskName2Class = {
